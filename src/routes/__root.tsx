1c0fd9b3fa85d3514927636680c195268e137361
Jongho Hong <myodan@pm.me>
2025-06-19 03:34:31 +0900

    250619

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -2,9 +2,12 @@ import type { QueryClient } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { createRootRouteWithContext, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
+import { useEffect } from "react";
 import { z } from "zod/v4";
+import { useShallow } from "zustand/react/shallow";
 import { useStompClientSync } from "~/hooks/use-stomp-client-sync";
 import { currentUserQueryOptions } from "~/queries/current-user-query";
+import { useStompClientStore } from "~/stores/stomp-client-store";
 
 type RootRouteContext = {
 	queryClient: QueryClient;
@@ -24,7 +27,32 @@ export const Route = createRootRouteWithContext<RootRouteContext>()({
 });
 
 function RouteComponent() {
+	const { currentUser } = Route.useRouteContext();
 	useStompClientSync();
+
+	const { isConnected, stompClient } = useStompClientStore(
+		useShallow(({ isConnected, stompClient }) => ({
+			isConnected,
+			stompClient,
+		})),
+	);
+
+	useEffect(() => {
+		if (!isConnected || !currentUser) {
+			return;
+		}
+
+		const userNotify = stompClient.subscribe(
+			"/user/notify",
+			(message: unknown) => {
+				console.log("User notification received:", message);
+			},
+		);
+
+		return () => {
+			userNotify.unsubscribe();
+		};
+	}, [isConnected, stompClient, currentUser]);
 
 	return (
 		<>
